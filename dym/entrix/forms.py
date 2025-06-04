433c666e0815d78a836904d4d79f8c9e1030067c
Oldrich Senkyr <senkyr@dknl.cz>
2025-06-04 14:52:11 +0200

    DYM-2025-06-02

---
+++ dym/entrix/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from agent.models import Person
+
+
+
+class PersonForm(forms.ModelForm):
+    class Meta:
+        model = Person
+        fields = ['unique_id', 'display_name', 'first_name', 'last_name', 'role', 'title_before', 'title_after']
+        widgets = {
+            'unique_id': forms.TextInput(attrs={'class': 'w-full p-2 border rounded'}),
+            'display_name': forms.TextInput(attrs={'class': 'w-full p-2 border rounded'}),
+            'first_name': forms.TextInput(attrs={'class': 'w-full p-2 border rounded'}),
+            'last_name': forms.TextInput(attrs={'class': 'w-full p-2 border rounded'}),
+            'role': forms.Select(attrs={'class': 'w-full p-2 border rounded'}),
+            'title_before': forms.TextInput(attrs={'class': 'w-full p-2 border rounded'}),
+            'title_after': forms.TextInput(attrs={'class': 'w-full p-2 border rounded'}),
+        }
+
+    def clean_unique_id(self):
+        unique_id = self.cleaned_data['unique_id']
+        if Person.objects.filter(unique_id__iexact=unique_id).exists():
+            raise forms.ValidationError("Toto ID osoby již existuje.")
+        return unique_id
+
+    def clean_display_name(self):
+        display_name = self.cleaned_data['display_name']
+        if Person.objects.filter(display_name__iexact=display_name).exists():
+            raise forms.ValidationError("Zobrazované jméno už existuje.")
+        return display_name
--- dym/entrix/views.py
+++ dym/entrix/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from agent.models import Person
-
-
-# Create your views here.
-
+from entrix.forms import PersonForm
 
 def persons_list(request):
-    query = request.GET.get("unique_id", "")
+    form = PersonForm()
     persons = Person.objects.all()
-    if query:
-        persons = persons.filter(unique_id__icontains=query)
-    return render(request, "entrix/persons-list.html", {"persons": persons})
 
+    if request.method == "POST":
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('entrix:persons_list')
+
+    unique_id = request.GET.get('unique_id')
+    if unique_id:
+        persons = persons.filter(unique_id__icontains=unique_id)
+
+    return render(request, 'entrix/persons-list.html', {
+        'form': form,
+        'persons': persons,
+    })
